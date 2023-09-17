1ca20bba98418c9c6541a2660d626fd14bbad677
Diego Honório <83922000+honoriio@users.noreply.github.com>
2023-09-17 18:25:36 -0300

    Add files via upload

---
+++ server/summarize.js
@@ -0,0 +1,21 @@
+﻿import { pipeline } from "@xenova/transformers"
+
+import { summaryExample } from "./utils/summary.js"
+
+export async function summarize(text) {
+  try {
+    //return summaryExample
+    
+    console.log("Realiazando o resumo...")
+
+    const generator = await pipeline("Summarization", "Xenova/distilbart-cnn-12-6" )
+
+    const output = await generator(text)
+
+    console.log("Resumo concluido com sucesso!")
+    return output[0].summary_text
+  } catch (error) {
+    console.log("Não foi possivel realizar o resumo", error)
+    throw new Error(error)
+  }
+}
